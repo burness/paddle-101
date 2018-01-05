e9ac672a781b41b5c8912661f5643d98df1085fc
burness <dss_1990@sina.com>
2018-01-05 22:00:46 +0800

    dd

--- script/deep_fm/data/download.sh
+++ script/deep_fm/data/download.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-export http_proxy=http://dev-proxy.oa.com:8080
-export https_proxy=$http_proxy
-export ftp_proxy=$http_proxy
-export all_proxy=$http_proxy
 wget --no-check-certificate https://s3-eu-west-1.amazonaws.com/criteo-labs/dac.tar.gz
 tar zxf dac.tar.gz
 rm -f dac.tar.gz
